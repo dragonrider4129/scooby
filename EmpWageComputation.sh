9323c4b68c52cec1a062874fd9a254d045b94190
dragonrider4129 <manojnaidugade99@gmail.com>
2020-12-11 18:53:47 +0530

    workhours

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,31 +1,47 @@
 #!/bin/bash
 echo "Welcome to Employee Wage Computation Program on Master Branch"
 
-echo "Welcome to Employee Wage Computation Program on Master Branch"
-
 
 IS_PART_TIME=1
 IS_FULL_TIME=2
+MAX_HRS_IN_MONTH=100
 EMP_RATE_PER_HR=20
-MAX_HRS_IN_MONTH=10
 NUM_WORKING_DAYS=20
 totalEmpHrs=0
 totalWorkingDays=0
-while(( $totalEmpHrs < $MAX_HRS_IN_MONTH && $totalWorkingDays < $NUM_WORKING_DAYS ))
-do
- ((totalWorkingDays++))
- echo "Total working day: $totalWorkingDays"
- random=$(( RANDOM%3 ))
-case $random in
- $IS_PART_TIME) empHrs=4
+totalWorkHours=0
+#Declaring dictionary
+declare -A dayWiseWages
+function calculateDailyWage()
+{
+ empHrs=$1
+ daySalary=$(( $empHrs * $EMP_RATE_PER_HR ))
+ echo $daySalary
+}
+function getWorkingHours()
+{
+ case $1 in
+ $IS_FULL_TIME ) empHrs=8
  ;;
- $IS_FULL_TIME) empHrs=8
+ $IS_PART_TIME ) empHrs=4
  ;;
  *) empHrs=0
+ ;;
  esac
- totalEmpHrs=$(( $totalEmpHrs + $empHrs ))
- echo "Total Employee Hours: $totalEmpHrs"
+ echo $empHrs
+}
+while(( $totalWorkHours < $MAX_HRS_IN_MONTH && $totalWorkingDays < $NUM_WORKING_DAYS ))
+do
+ ((totalWorkingDays++))
+ #Calling function getWorkingHours by passing random number
+ workHours="$( getWorkingHours $(( RANDOM%3 )) )"
+ echo "Work hours from function getWorkingHours()::::::::::>>> $workHours "
+ totalWorkHours=$(( $totalWorkHours + $workHours ))
+ echo -e "TotalWorkHours: $totalWorkHours\n"
+ #Using array to store day-wise salary
+ empDailyWage[$totalWorkingDays]="$( calculateDailyWage $workHours )"
+ #Using dictionary to store day-wise salary
+ dayWiseWages[$totalWorkingDays]="$(calculateDailyWage $workHours)"
 done
-totalSalary=$(( $totalEmpHrs * $EMP_RATE_PER_HR))
-echo "Total Salary: $totalSalary"
-
+totalSalary=$(( $totalWorkHours * $EMP_RATE_PER_HR ));
+echo "Total salary: $totalSalary"
